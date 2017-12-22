f8ce70f6636968a37ee3069ddfdab609083afb7a
Ivan Gonzalez <ivan@Ivans-MacBook-Pro.local>
2017-12-21 16:08:51 -0800

    basic email works

--- routes/email.js
+++ routes/email.js
@@ -1,13 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const config = require('../config.json');
 
 router.get('/', function(req, res, next) {
   res.send('hello world');
 });
 
 router.post('/', function(req, res, next) {
-  console.log('handling post');
-  res.send('hello, in post');
+  let to = req.body.to;
+  let subject = req.body.subject;
+  let message = req.body.message;
+  let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: config.email,
+        pass: config.pass
+    }
+  });
+
+  var mailOptions = {
+    from: config.email,
+    to: to, 
+    subject: subject,
+    text: message
+  }
+
+  transporter.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        res.send('ok');
+    }
+  });
 });
 
 module.exports = router;
